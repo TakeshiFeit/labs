83f06d0124f31274ff24679a715191e0e634340e
TakeshiFeit <feit.and@mail.ru>
2024-11-19 13:09:54 +0400

    Update oneMasWithRe.py

--- lab4/oneMasWithRe/oneMasWithRe/oneMasWithRe.py
+++ lab4/oneMasWithRe/oneMasWithRe/oneMasWithRe.py
@@ -1,4 +1,4 @@
-﻿# Написать программу, которая читая символы из файла, распознает, преобразует и 
+# Написать программу, которая читая символы из файла, распознает, преобразует и 
 # выводит на экран объекты по определенному правилу. Объекты разделены 
 # пробелами. Распознавание и преобразование делать по возможности через 
 # регулярные выражения. Для упрощения под выводом числа прописью 
@@ -8,7 +8,7 @@
 import re
 
 file = open('text.txt', encoding="utf8")
-num = re.compile(r"\d+")
+q = re.compile(r"\d+")
 
 num2some = ('zero', 
             'one', 
@@ -27,42 +27,38 @@ num2some = ('zero',
             '.' * 14,  #E
             '.' * 15)  #F
 
-something = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', '-') 
+def check():
+    cond = False
+    while cond == False:
+        txt = file.readline().split()
+        num = []
+        for item in txt:
+            try:
+                q = int(item, 16)
+                num.append(item)
+                cond = True
+            except ValueError:
+                pass
+    return num
 
-cond = False
-while cond == False:
-    txt = file.readline().split()
-    for item in txt:
-        try:
-            Max = int(item, 16) - 1 
-            cond = True
-            break
-        except ValueError:
-            pass
+num = check()
+Max = int(num[0], 16) - 1
 
-while txt != []:
-    for i in range(len(txt)):
-        a = 0
-        for j in (txt[i]):
-            if  str(j) not in (something):
-                a += 1
-                continue
-        if a != 0:
-            continue
-        if int(txt[i], 16) > 0:
-            txt[i] = "+" + txt[i]
+while num != []:
+    for i in range(len(num)):
+        if int(num[i], 16) > 0:
+            num[i] = "+" + num[i]
             sign = ""
         else:
             sign = "-"
         cond = False
-        if Max < int(txt[i],16):
-            Max = int(txt[i],16)
+        if Max < int(num[i],16):
+            Max = int(num[i],16)
             cond = True
         replaced = ""
-        txt2 = re.sub("(.)", r'\1 ', str(txt[i]))
+        txt2 = re.sub("(.)", r'\1 ', str(num[i]))
         txt2 = [int(txt2[x], 16) for x in range(2, len(txt2), 2)]
-        replaced += num.sub(lambda x: num2some[int(x.group())], str(txt2))
-        txt[i] = replaced[:1] + sign + replaced[1:]
-        print (txt[i]) if (cond == True) else()
-    txt = file.readline().split()
-
+        replaced += q.sub(lambda x: num2some[int(x.group())], str(txt2))
+        num[i] = replaced[:1] + sign + replaced[1:]
+        print (num[i]) if (cond == True) else()
+    num = check()
